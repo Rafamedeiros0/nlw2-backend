9e3a3bc7f9020a7fdf9c5da7720eb980ae0c0933
RafaMedeiros0 <rafamedeiros0@gmail.com>
2020-08-06 00:41:17 -0300

    fix migration create-connection and create route connections

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -85,7 +85,7 @@ export default class ClassesController {
       return response.status(201).send()
     } catch (err) {
       await trx.rollback()
-  
+
       return response.status(400).json({
         error: 'Unexpected error while creating new class'
       })
